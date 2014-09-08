983eff82a115caa2bd2df326207e84e22756bb72
Majid-Kaffash <majidrobo@gmail.com>
2014-09-08 21:03:20 +0430

    Problem 1

--- 1.cpp
+++ 1.cpp
@@ -1,20 +1,11 @@
 #include<iostream>
-
 using namespace std;
-
 int main() {
-
 	int sum = 0;
-
 	for(unsigned int i = 1; i < 1000; ++i) {
-
 		if(i % 3 == 0 || i % 5 == 0)
-			sum += i;
-
-		}
-
+		sum += i;
+	}
 	cout << sum << endl;
-
 	return 0;
-
 }
